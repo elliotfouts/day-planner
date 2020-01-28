617035387f5334e09d8231ba64e1d2c86b989343
Elliot Fouts <elliotfouts@gmail.com>
2020-01-28 15:13:00 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// 1. create markup for two buttons
+// 2. create a css class that changes the image based on your desired power
+// 3. add event listener to the button in javascript 
+// 4. "onclick" toggle css class 
